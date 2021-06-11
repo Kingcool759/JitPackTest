b17348f5fe6aadc4be983af7be205f526262bdf9
armStrong759 <3058587756@qq.com>
2021-06-11 16:41:07 +0800

    gogogo

--- first/src/main/java/com/kc/first/User.kt
+++ first/src/main/java/com/kc/first/User.kt
@@ -1,5 +1,8 @@
 package com.kc.first
 
+import com.example.mykotlindemo.utils.toast
+import com.kc.library.base.utils.DateUtil
+
 /**
  * @data on 2021/6/11 3:19 下午
  * @auther KC
@@ -8,6 +11,6 @@ package com.kc.first
 class User {
     var name = ""
     fun callSomeBody(){
-
+        toast("hhh")
     }
 }
